d93a3f42676c3a5d4ef95c749dfe412e24d346a2
lissuzhang <lissuzhang>
2016-02-06 18:29:34 -0800

    Click player score to inc by 1

--- app/src/main/java/com/laudev/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/laudev/android/scorekeeper/MainActivity.java
@@ -2,12 +2,37 @@ package com.laudev.android.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int playerOneScore = 0;
+    int playerTwoScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void incPlayerOne (View view) {
+        playerOneScore += 1;
+        displayPlayerOne(playerOneScore);
+    }
+
+    public void incPlayerTwo (View view) {
+        playerTwoScore += 1;
+        displayPlayerTwo(playerTwoScore);
+    }
+
+    private void displayPlayerOne (int score) {
+        TextView view = (TextView) findViewById(R.id.player_one_score);
+        view.setText("" + score);
+    }
+
+    private void displayPlayerTwo (int score) {
+        TextView view = (TextView) findViewById(R.id.player_two_score);
+        view.setText("" + score);
     }
 }
