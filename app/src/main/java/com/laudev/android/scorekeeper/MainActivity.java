e4fb9e1d695b01cbc4ee4dd3a458e85db3d7cdbb
lissuzhang <lissuzhang>
2016-02-06 20:14:13 -0800

    Create toggle plus and minus for change increment

--- app/src/main/java/com/laudev/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/laudev/android/scorekeeper/MainActivity.java
@@ -9,11 +9,23 @@ public class MainActivity extends AppCompatActivity {
 
     int playerOneScore = 0;
     int playerTwoScore = 0;
+    boolean increment = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeIncrement(View view) {
+        TextView incrementAmount = (TextView) findViewById(R.id.increment_amount);
+        if (increment) {
+            increment = false;
+            incrementAmount.setText("Tap each player to +1");
+        } else {
+            increment = true;
+            incrementAmount.setText("Tap each player to -1");
+        }
     }
 
     public void incPlayerOne (View view) {
